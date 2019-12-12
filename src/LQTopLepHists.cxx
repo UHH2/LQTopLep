e7308110ebd1b91348f3d07048760d90b8236d6c
Arne Reimers <Arne.Reimers@desy.de>
2019-12-12 19:19:41 +0100

    Made Trigger-Preselection, cleaned code, made it more modular

--- src/LQTopLepHists.cxx
+++ src/LQTopLepHists.cxx
@@ -1,8 +1,11 @@
-#include "UHH2/LQTopLep/include/LQTopLepHists.h"
+#include "../include/LQTopLepHists.h"
 #include "UHH2/core/include/Event.h"
+#include "UHH2/common/include/Utils.h"
+#include "UHH2/common/include/JetIds.h"
+#include <math.h>
 
 #include "TH1F.h"
-#include "TH2F.h"
+#include "TH2D.h"
 #include <iostream>
 
 using namespace std;
@@ -10,134 +13,300 @@ using namespace uhh2;
 using namespace uhh2examples;
 
 LQTopLepHists::LQTopLepHists(Context & ctx, const string & dirname): Hists(ctx, dirname){
-  // book all histograms here
+
   // jets
-  book<TH1F>("N_jets", "N_{jets}", 20, 0, 20);  
-  book<TH1F>("pt_jets", "p_{T}^{jets} [GeV]", 50, 0, 1600); // pt of all jets in one histogram
-  book<TH1F>("eta_jets", "#eta jets", 40, -2.5, 2.5);
+  book<TH1F>("N_jets", "N_{jets}", 21, -0.5, 20.5);
+  book<TH1F>("pt_jet", "p_{T}^{jets} [GeV]", 50, 0, 1500);
+  book<TH1F>("pt_jet1", "p_{T}^{jet 1} [GeV]", 50, 0, 1500);
+  book<TH1F>("pt_jet2", "p_{T}^{jet 2} [GeV]", 50, 0, 1500);
+  book<TH1F>("pt_jet3", "p_{T}^{jet 3} [GeV]", 50, 0, 1500);
+  book<TH1F>("eta_jet", "#eta^{jets}", 50, -2.5, 2.5);
+  book<TH1F>("eta_jet1", "#eta^{jet 1}", 50, -2.5, 2.5);
+  book<TH1F>("eta_jet2", "#eta^{jet 2}", 50, -2.5, 2.5);
+  book<TH1F>("eta_jet3", "#eta^{jet 3}", 50, -2.5, 2.5);
+  book<TH1F>("phi_jet", "#phi^{jets}", 35, -3.5, 3.5);
+  book<TH1F>("phi_jet1", "#phi^{jet 1}", 35, -3.5, 3.5);
+  book<TH1F>("phi_jet2", "#phi^{jet 2}", 35, -3.5, 3.5);
+  book<TH1F>("phi_jet3", "#phi^{jet 3}", 35, -3.5, 3.5);
+  book<TH1F>("m_jet", "m^{jets}", 50, 0, 500);
+  book<TH1F>("m_jet1", "m^{jet 1}", 50, 0, 500);
+  book<TH1F>("m_jet2", "m^{jet 2}", 50, 0, 500);
+  book<TH1F>("m_jet3", "m^{jet 3}", 50, 0, 500);
+  book<TH1F>("csv_jet", "CSV^{jets}", 20, 0, 1);
+  book<TH1F>("csv_jet1", "CSV^{jet 1}", 20, 0, 1);
+  book<TH1F>("csv_jet2", "CSV^{jet 2}", 20, 0, 1);
+  book<TH1F>("csv_jet3", "CSV^{jet 3}", 20, 0, 1);
+  book<TH1F>("N_bJets_loose", "N_{jets}^{CSV loose}", 11, -0.5, 10.5);
+  book<TH1F>("N_bJets_med", "N_{jets}^{CSV medium}", 11, -0.5, 10.5);
+  book<TH1F>("N_bJets_tight", "N_{jets}^{CSV tight}", 11, -0.5, 10.5);
+  book<TH1F>("N_deepjet_loose", "N_{jets}^{DeepJet loose}", 11, -0.5, 10.5);
+  book<TH1F>("N_deepjet_med", "N_{jets}^{DeepJet medium}", 11, -0.5, 10.5);
+  book<TH1F>("N_deepjet_tight", "N_{jets}^{DeepJet tight}", 11, -0.5, 10.5);
 
-  book<TH1F>("N_PU", "N_{PU}", 100, 0, 100);  
-  book<TH1F>("eta_jet1", "#eta^{jet 1}", 40, -2.5, 2.5);
-  book<TH1D>("pt_jet1", "#p_{T}^{jet 1}", 100, 0, 3000);
-  book<TH1D>("pt_jet2", "#p_{T}^{jet 2}", 100, 0, 3000);
-  book<TH1D>("pt_jet3", "#p_{T}^{jet 3}", 100, 0, 3000);
-  book<TH1F>("eta_jet2", "#eta^{jet 2}", 40, -2.5, 2.5);
-  book<TH1F>("eta_jet3", "#eta^{jet 3}", 40, -2.5, 2.5);
-  book<TH1F>("eta_jet4", "#eta^{jet 4}", 40, -2.5, 2.5);
 
-
-
-  // muons
-  book<TH1F>("N_mu", "N^{#mu}", 10, 0, 10);
-  book<TH1F>("pt_mu", "p_{T}^{#mu} [GeV/c]", 40, 0, 200);
-  book<TH1F>("eta_mu", "#eta^{#mu}", 40, -2.1, 2.1);
+  book<TH1F>("N_mu", "N^{#mu}", 11, -0.5, 10.5);
+  book<TH1F>("pt_mu", "p_{T}^{#mu} [GeV]", 50, 0, 1500);
+  book<TH1F>("pt_mu1", "p_{T}^{#mu 1} [GeV]", 50, 0, 1500);
+  book<TH1F>("pt_mu2", "p_{T}^{#mu 2} [GeV]", 50, 0, 1500);
+  book<TH1F>("eta_mu", "#eta^{#mu}", 50, -2.5, 2.5);
+  book<TH1F>("eta_mu1", "#eta^{#mu 1}", 50, -2.5, 2.5);
+  book<TH1F>("eta_mu2", "#eta^{#mu 2}", 50, -2.5, 2.5);
+  book<TH1F>("phi_mu", "#phi^{#mu}", 35, -3.5, 3.5);
+  book<TH1F>("phi_mu1", "#phi^{#mu 1}", 35, 3.5, 3.5);
+  book<TH1F>("phi_mu2", "#phi^{#mu 2}", 35, 3.5, 3.5);
   book<TH1F>("reliso_mu", "#mu rel. Iso", 40, 0, 0.5);
+  book<TH1F>("reliso_mu1", "#mu 1 rel. Iso", 40, 0, 0.5);
+  book<TH1F>("reliso_mu2", "#mu 2 rel. Iso", 40, 0, 0.5);
+  book<TH1F>("reliso_mu_rebin", "#mu rel. Iso ", 400, 0, 5);
+  book<TH1F>("reliso_mu1_rebin", "#mu 1 rel. Iso ", 400, 0, 5);
+  book<TH1F>("reliso_mu2_rebin", "#mu 2 rel. Iso ", 400, 0, 5);
+  book<TH1F>("N_ele", "N^{e}", 11, -0.5, 10.5);
+  book<TH1F>("pt_ele", "p_{T}^{e} [GeV]", 50, 0, 1500);
+  book<TH1F>("pt_ele1", "p_{T}^{e 1} [GeV]", 50, 0, 1500);
+  book<TH1F>("pt_ele2", "p_{T}^{e 2} [GeV]", 50, 0, 1500);
+  book<TH1F>("eta_ele", "#eta^{e}", 50, -2.5, 2.5);
+  book<TH1F>("eta_ele1", "#eta^{ele 1}", 50, -2.5, 2.5);
+  book<TH1F>("eta_ele2", "#eta^{ele 2}", 50, -2.5, 2.5);
+  book<TH1F>("phi_ele", "#phi^{e}", 35, -3.5, 3.5);
+  book<TH1F>("phi_ele1", "#phi^{e 1}", 35, -3.5, 3.5);
+  book<TH1F>("phi_ele2", "#phi^{e 2}", 35, -3.5, 3.5);
+  book<TH1F>("reliso_ele", "e rel. Iso", 40, 0, 0.5);
+  book<TH1F>("reliso_ele1", "e 1 rel. Iso", 40, 0, 0.5);
+  book<TH1F>("reliso_ele2", "e 2 rel. Iso", 40, 0, 0.5);
+  book<TH1F>("reliso_ele_rebin", "e rel. Iso", 400, 0, 5);
+  book<TH1F>("reliso_ele1_rebin", "e 1 rel. Iso", 400, 0, 5);
+  book<TH1F>("reliso_ele2_rebin", "e 2 rel. Iso", 400, 0, 5);
+  book<TH1F>("M_mumu", "M_{#mu#mu} [GeV]",75 , 0, 500);
+  book<TH1F>("M_ee", "M_{ee} [GeV]",75 , 0, 500);
 
-  // electrons
-  
-  book<TH1F>("N_e", "number of electrons", 9, -0.5, 8.5);
-  book<TH1F>("pt_e", "p_{T}^{e} [GeV]", 50, 0, 1600);
-  book<TH1F>("eta_e", "#eta electrons", 40, -2.1, 2.1);
-  book<TH1F>("reliso_e", "e rel. Iso", 40, 0, 0.5);
 
+  book<TH1F>("NPV", "number of primary vertices", 91, -0.50, 90.5);
+  book<TH1F>("MET", "missing E_{T} [GeV]", 50, 0, 7000);
+  book<TH1F>("MET_rebin", "missing E_{T} [GeV]", 50, 0, 1500);
+  book<TH1F>("MET_rebin2", "missing E_{T} [GeV]", 30, 0, 1500);
+  book<TH1F>("MET_rebin3", "missing E_{T} [GeV]", 15, 0, 1500);
+  book<TH1F>("ST", "S_{T} [GeV]", 50, 0, 7000);
+  book<TH1F>("ST_rebin", "S_{T} [GeV]", 200, 0, 5000);
+  book<TH1F>("ST_rebin2", "S_{T} [GeV]", 100, 0, 5000);
+  book<TH1F>("ST_rebin3", "S_{T} [GeV]", 50, 0, 5000);
+  book<TH1F>("STjets", "S_{T}^{jets} [GeV]", 50, 0, 7000);
+  book<TH1F>("STjets_rebin", "S_{T}^{jets} [GeV]", 200, 0, 5000);
+  book<TH1F>("STjets_rebin2", "S_{T}^{jets} [GeV]", 100, 0, 5000);
+  book<TH1F>("STjets_rebin3", "S_{T}^{jets} [GeV]", 50, 0, 5000);
+  book<TH1F>("STlep", "S_{T}^{lep} [GeV]", 50, 0, 7000);
+  book<TH1F>("STlep_rebin", "S_{T}^{lep} [GeV]", 50, 0, 1500);
+  book<TH1F>("STlep_rebin2", "S_{T}^{lep} [GeV]", 30, 0, 1500);
+  book<TH1F>("STlep_rebin3", "S_{T}^{lep} [GeV]", 15, 0, 1500);
 
-  // primary vertices
-  book<TH1F>("N_pv", "N^{PV}", 50, 0, 50);
+  book<TH1F>("sum_event_weights", "BinContent = sum(eventweights)", 1, 0.5, 1.5);
 
-  // general
-  book<TH1D>("S_T", "S_{T}", 100, 0, 5000);
-  book<TH1D>("sum_event_weights", "BinContent = sum(eventweights)", 1, 0.5, 1.5);
+  is_mc = ctx.get("dataset_type") == "MC";
+
 }
 
 
 void LQTopLepHists::fill(const Event & event){
-  // fill the histograms. Please note the comments in the header file:
-  // 'hist' is used here a lot for simplicity, but it will be rather
-  // slow when you have many histograms; therefore, better
-  // use histogram pointers as members as in 'UHH2/common/include/ElectronHists.h'
-  
-  // Don't forget to always use the weight when filling.
+
   double weight = event.weight;
-  
-  std::vector<Jet>* jets = event.jets;
-  int Njets = jets->size();
-  hist("N_jets")->Fill(Njets, weight);
-  if(!event.isRealData)  hist("N_PU")->Fill(event.genInfo->pileup_TrueNumInteractions(), weight);
-
-  for(int i=0; i <Njets; i++) {
-    hist("pt_jets")->Fill(jets->at(i).pt(), weight);
-    hist("eta_jets")->Fill(jets->at(i).eta(), weight);
-  }
-  if(Njets>=1){
-    hist("eta_jet1")->Fill(jets->at(0).eta(), weight);
-  }
-  if(Njets>=2){
-    hist("eta_jet2")->Fill(jets->at(1).eta(), weight);
-  }
-  if(Njets>=3){
-    hist("eta_jet3")->Fill(jets->at(2).eta(), weight);
-  }
-  if(Njets>=4){
-    hist("eta_jet4")->Fill(jets->at(3).eta(), weight);
-  }
-  if(Njets>=1){
-    hist("pt_jet1")->Fill(jets->at(0).pt(), weight);
-  }
-  if(Njets>=2){
-    hist("pt_jet2")->Fill(jets->at(1).pt(), weight);
-  }
-  if(Njets>=3){
-    hist("pt_jet3")->Fill(jets->at(2).pt(), weight);
-  }
-
-  int Nmuons = event.muons->size();
-  hist("N_mu")->Fill(Nmuons, weight);
-  for (const Muon & thismu : *event.muons){
-      hist("pt_mu")->Fill(thismu.pt(), weight);
-      hist("eta_mu")->Fill(thismu.eta(), weight);
-      hist("reliso_mu")->Fill(thismu.relIso(), weight);
-  } 
-  // Electrons  
-  //std::vector<Electron>* electronVector = event.electrons;
-
-  const int Nele = event.electrons->size();
-  hist("N_e")->Fill(Nele, weight);
-  for (const Electron & thise : *event.electrons) {
-    hist("pt_e")->Fill(thise.pt(), weight);
-    hist("eta_e")->Fill(thise.eta(), weight);
-    hist("reliso_e")->Fill(thise.relIso(), weight);
-  }
-    //ST
-  auto met = event.met->pt();
-  double st = 0.0;
-  double st_jets = 0.0;
-  double st_lep = 0.0;
-  for(const auto & jet : *event.jets){
-    st_jets += jet.pt();
-  }
-  //Bedeutung der for-Schleife
-  /*const auto jets = event.jets;
-    for(unsigned int i=0; i<jets.size();i++){
-    Jet jet=jets[i];
-    st +=jet.pt();
-    }*/
-  for(const auto & electron : *event.electrons){
-    st_lep += electron.pt();
-  }
-  for(const auto & muon : *event.muons){
-    st_lep += muon.pt();
-  }
-
-  st = st_lep + st_jets + met;
-  hist("S_T")->Fill(st, weight);
-
-  int Npvs = event.pvs->size();
-  hist("N_pv")->Fill(Npvs, weight);
-
-  //sum_event_weights
   hist("sum_event_weights")->Fill(1., weight);
 
 
-}
+  /*
+  █      ██ ███████ ████████ ███████
+  █      ██ ██         ██    ██
+  █      ██ █████      ██    ███████
+  █ ██   ██ ██         ██         ██
+  █  █████  ███████    ██    ███████
+  */
+
+
+  vector<Jet>* jets = event.jets;
+  int Njets = jets->size();
+  hist("N_jets")->Fill(Njets, weight);
+
+  for(unsigned int i=0; i<jets->size(); i++){
+    hist("pt_jet")->Fill(jets->at(i).pt(),weight);
+    hist("eta_jet")->Fill(jets->at(i).eta(),weight);
+    hist("phi_jet")->Fill(jets->at(i).phi(),weight);
+    hist("m_jet")->Fill(jets->at(i).v4().M(),weight);
+    hist("csv_jet")->Fill(jets->at(i).btag_combinedSecondaryVertex(), weight);
+    if(i==0){
+      hist("pt_jet1")->Fill(jets->at(i).pt(),weight);
+      hist("eta_jet1")->Fill(jets->at(i).eta(),weight);
+      hist("phi_jet1")->Fill(jets->at(i).phi(),weight);
+      hist("m_jet1")->Fill(jets->at(i).v4().M(),weight);
+      hist("csv_jet1")->Fill(jets->at(i).btag_combinedSecondaryVertex(), weight);
+    }
+    else if(i==1){
+      hist("pt_jet2")->Fill(jets->at(i).pt(),weight);
+      hist("eta_jet2")->Fill(jets->at(i).eta(),weight);
+      hist("phi_jet2")->Fill(jets->at(i).phi(),weight);
+      hist("m_jet2")->Fill(jets->at(i).v4().M(),weight);
+      hist("csv_jet2")->Fill(jets->at(i).btag_combinedSecondaryVertex(), weight);
+    }
+    else if(i==2){
+      hist("pt_jet3")->Fill(jets->at(i).pt(),weight);
+      hist("eta_jet3")->Fill(jets->at(i).eta(),weight);
+      hist("phi_jet3")->Fill(jets->at(i).phi(),weight);
+      hist("m_jet3")->Fill(jets->at(i).v4().M(),weight);
+      hist("csv_jet3")->Fill(jets->at(i).btag_combinedSecondaryVertex(), weight);
+    }
+  }
+
+  int Nbjets_loose = 0, Nbjets_medium = 0, Nbjets_tight = 0;
+  int Ndeepjet_loose = 0, Ndeepjet_med = 0, Ndeepjet_tight = 0;
+  CSVBTag Btag_loose = CSVBTag(CSVBTag::WP_LOOSE);
+  CSVBTag Btag_medium = CSVBTag(CSVBTag::WP_MEDIUM);
+  CSVBTag Btag_tight = CSVBTag(CSVBTag::WP_TIGHT);
+
+  JetId DeepjetLoose = BTag(BTag::DEEPJET, BTag::WP_LOOSE);
+  JetId DeepjetMedium = BTag(BTag::DEEPJET, BTag::WP_MEDIUM);
+  JetId DeepjetTight = BTag(BTag::DEEPJET, BTag::WP_TIGHT);
+
+  for (unsigned int i =0; i<jets->size(); i++) {
+    if(Btag_loose(jets->at(i),event))  Nbjets_loose++;
+    if(Btag_medium(jets->at(i),event)) Nbjets_medium++;
+    if(Btag_tight(jets->at(i),event))  Nbjets_tight++;
+    if(DeepjetLoose(jets->at(i),event))  Ndeepjet_loose++;
+    if(DeepjetMedium(jets->at(i),event)) Ndeepjet_med++;
+    if(DeepjetTight(jets->at(i),event))  Ndeepjet_tight++;
+  }
+
+  hist("N_bJets_loose")->Fill(Nbjets_loose,weight);
+  hist("N_bJets_med")->Fill(Nbjets_medium,weight);
+  hist("N_bJets_tight")->Fill(Nbjets_tight,weight);
+  hist("N_deepjet_loose")->Fill(Ndeepjet_loose,weight);
+  hist("N_deepjet_med")->Fill(Ndeepjet_med,weight);
+  hist("N_deepjet_tight")->Fill(Ndeepjet_tight,weight);
+
+
+  /*
+  ███    ███ ██    ██  ██████  ███    ██ ███████
+  ████  ████ ██    ██ ██    ██ ████   ██ ██
+  ██ ████ ██ ██    ██ ██    ██ ██ ██  ██ ███████
+  ██  ██  ██ ██    ██ ██    ██ ██  ██ ██      ██
+  ██      ██  ██████   ██████  ██   ████ ███████
+  */
+
+
+  vector<Muon>* muons = event.muons;
+  int Nmuons = muons->size();
+  hist("N_mu")->Fill(Nmuons, weight);
+
+  for(int i=0; i<Nmuons; i++){
+
+    hist("pt_mu")->Fill(muons->at(i).pt(),weight);
+    hist("eta_mu")->Fill(muons->at(i).eta(),weight);
+    hist("phi_mu")->Fill(muons->at(i).phi(),weight);
+    hist("reliso_mu")->Fill(muons->at(i).relIso(),weight);
+    hist("reliso_mu_rebin")->Fill(muons->at(i).relIso(),weight);
+    if(i==0){
+      hist("pt_mu1")->Fill(muons->at(i).pt(),weight);
+      hist("eta_mu1")->Fill(muons->at(i).eta(),weight);
+      hist("phi_mu1")->Fill(muons->at(i).phi(),weight);
+      hist("reliso_mu1")->Fill(muons->at(i).relIso(),weight);
+      hist("reliso_mu1_rebin")->Fill(muons->at(i).relIso(),weight);
+    }
+    else if(i==1){
+      hist("pt_mu2")->Fill(muons->at(i).pt(),weight);
+      hist("eta_mu2")->Fill(muons->at(i).eta(),weight);
+      hist("phi_mu2")->Fill(muons->at(i).phi(),weight);
+      hist("reliso_mu2")->Fill(muons->at(i).relIso(),weight);
+      hist("reliso_mu2_rebin")->Fill(muons->at(i).relIso(),weight);
+    }
+  }
+
+  for(int i=0; i<Nmuons; i++){
+    for(int j=0; j<Nmuons; j++){
+      if(j <= i) continue;
+      hist("M_mumu")->Fill((muons->at(i).v4() + muons->at(j).v4()).M() ,weight);
+    }
+  }
+
+  /*
+  ███████ ██      ███████  ██████ ████████ ██████   ██████  ███    ██ ███████
+  ██      ██      ██      ██         ██    ██   ██ ██    ██ ████   ██ ██
+  █████   ██      █████   ██         ██    ██████  ██    ██ ██ ██  ██ ███████
+  ██      ██      ██      ██         ██    ██   ██ ██    ██ ██  ██ ██      ██
+  ███████ ███████ ███████  ██████    ██    ██   ██  ██████  ██   ████ ███████
+  */
+
+
+  vector<Electron>* electrons = event.electrons;
+  int Nelectrons = electrons->size();
+  hist("N_ele")->Fill(Nelectrons, weight);
+
+  for(int i=0; i<Nelectrons; i++){
+    hist("pt_ele")->Fill(electrons->at(i).pt(),weight);
+    hist("eta_ele")->Fill(electrons->at(i).eta(),weight);
+    hist("phi_ele")->Fill(electrons->at(i).phi(),weight);
+    hist("reliso_ele")->Fill(electrons->at(i).relIso(),weight);
+    hist("reliso_ele_rebin")->Fill(electrons->at(i).relIso(),weight);
+    if(i==0){
+      hist("pt_ele1")->Fill(electrons->at(i).pt(),weight);
+      hist("eta_ele1")->Fill(electrons->at(i).eta(),weight);
+      hist("phi_ele1")->Fill(electrons->at(i).phi(),weight);
+      hist("reliso_ele1")->Fill(electrons->at(i).relIso(),weight);
+      hist("reliso_ele1_rebin")->Fill(electrons->at(i).relIso(),weight);
+    }
+    else if(i==1){
+      hist("pt_ele2")->Fill(electrons->at(i).pt(),weight);
+      hist("eta_ele2")->Fill(electrons->at(i).eta(),weight);
+      hist("phi_ele2")->Fill(electrons->at(i).phi(),weight);
+      hist("reliso_ele2")->Fill(electrons->at(i).relIso(),weight);
+      hist("reliso_ele2_rebin")->Fill(electrons->at(i).relIso(),weight);
+    }
+  }
+
+  for(int i=0; i<Nelectrons; i++){
+    for(int j=0; j<Nelectrons; j++){
+      if(j <= i) continue;
+      hist("M_ee")->Fill((electrons->at(i).v4() + electrons->at(j).v4()).M() ,weight);
+    }
+  }
+
+
+
+  /*
+  ██████  ███████ ███    ██ ███████ ██████   █████  ██
+  ██      ██      ████   ██ ██      ██   ██ ██   ██ ██
+  ██  ███ █████   ██ ██  ██ █████   ██████  ███████ ██
+  ██   ██ ██      ██  ██ ██ ██      ██   ██ ██   ██ ██
+  ██████  ███████ ██   ████ ███████ ██   ██ ██   ██ ███████
+  */
+
+
+
+  int Npvs = event.pvs->size();
+  hist("NPV")->Fill(Npvs, weight);
+
+  double met = event.met->pt();
+  double st = 0., st_jets = 0., st_lep = 0.;
+  for(const auto & jet : *jets)           st_jets += jet.pt();
+  for(const auto & electron : *electrons) st_lep += electron.pt();
+  for(const auto & muon : *muons)         st_lep += muon.pt();
+  st = st_jets + st_lep + met;
+
+  hist("MET")->Fill(met, weight);
+  hist("MET_rebin")->Fill(met, weight);
+  hist("MET_rebin2")->Fill(met, weight);
+  hist("MET_rebin3")->Fill(met, weight);
+  hist("ST")->Fill(st, weight);
+  hist("ST_rebin")->Fill(st, weight);
+  hist("ST_rebin2")->Fill(st, weight);
+  hist("ST_rebin3")->Fill(st, weight);
+  hist("STjets")->Fill(st_jets, weight);
+  hist("STjets_rebin")->Fill(st_jets, weight);
+  hist("STjets_rebin2")->Fill(st_jets, weight);
+  hist("STjets_rebin3")->Fill(st_jets, weight);
+  hist("STlep")->Fill(st_lep, weight);
+  hist("STlep_rebin")->Fill(st_lep, weight);
+  hist("STlep_rebin2")->Fill(st_lep, weight);
+  hist("STlep_rebin3")->Fill(st_lep, weight);
+
+
+} //Methode
+
+
 
 LQTopLepHists::~LQTopLepHists(){}
---
+++ src/LQTopLepTriggerPreselectionModule.cxx
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <memory>
+
+#include "UHH2/core/include/AnalysisModule.h"
+#include "UHH2/core/include/Event.h"
+#include "UHH2/common/include/CommonModules.h"
+#include "UHH2/common/include/CleaningModules.h"
+//hists
+#include "UHH2/common/include/ElectronHists.h"
+#include "UHH2/common/include/MuonHists.h"
+#include "UHH2/common/include/JetHists.h"
+#include "UHH2/common/include/LuminosityHists.h"
+#include "UHH2/common/include/EventHists.h"
+//selections
+#include "UHH2/common/include/LumiSelection.h"
+#include "UHH2/common/include/NSelections.h"
+#include "UHH2/common/include/TriggerSelection.h"
+//IDs
+#include "UHH2/common/include/ElectronIds.h"
+#include "UHH2/common/include/MuonIds.h"
+// my own classes
+#include "UHH2/LQTopLep/include/LQTopLepSelections.h"
+#include "UHH2/LQTopLep/include/LQTopLepHists.h"
+//#include "UHH2/LQTopLep/include/LQTopLepModules.h"
+#include <UHH2/SingleTth/include/ModuleBASE.h>
+
+
+using namespace std;
+using namespace uhh2;
+
+namespace uhh2examples {
+
+  /** \brief Basic analysis example of an AnalysisModule (formerly 'cycle') in UHH2
+  *
+  * This is the central class which calls other AnalysisModules, Hists or Selection classes.
+  * This AnalysisModule, in turn, is called (via AnalysisModuleRunner) by SFrame.
+  */
+  class LQTopLepTriggerPreselectionModule: public ModuleBASE {
+  public:
+
+    explicit LQTopLepTriggerPreselectionModule(Context & ctx);
+    virtual bool process(Event & event) override;
+    void book_histograms(uhh2::Context&, vector<string>);
+    void fill_histograms(uhh2::Event&, string);
+
+  private:
+
+    std::unique_ptr<CommonModules> common;
+
+    // declare the Selections to use. Use unique_ptr to ensure automatic call of delete in the destructor,
+    // to avoid memory leaks.
+    std::unique_ptr<Selection> nelectrons_lowpt_sel, nelectrons_highpt_sel, nmuons_lowpt_sel, nmuons_highpt_sel, njets_sel;
+
+    ElectronId eleId_lowpt, eleId_highpt;
+    MuonId muId_lowpt, muId_highpt;
+    JetId jetId;
+
+    bool is_mc;
+
+
+  };
+
+  void LQTopLepTriggerPreselectionModule::book_histograms(uhh2::Context& ctx, vector<string> tags){
+    for(const auto & tag : tags){
+      cout << "booking histograms with tag " << tag << endl;
+      string mytag = tag+"_General";
+      book_HFolder(mytag, new LQTopLepHists(ctx,mytag));
+      mytag = tag+"_Muons";
+      book_HFolder(mytag, new MuonHists(ctx,mytag));
+      mytag = tag+"_Electrons";
+      book_HFolder(mytag, new ElectronHists(ctx,mytag));
+      mytag = tag+"_Jets";
+      book_HFolder(mytag, new JetHists(ctx,mytag));
+      mytag = tag+"_Event";
+      book_HFolder(mytag, new EventHists(ctx,mytag));
+    }
+  }
+
+  void LQTopLepTriggerPreselectionModule::fill_histograms(uhh2::Event& event, string tag){
+    string mytag = tag+"_General";
+    HFolder(mytag)->fill(event);
+    mytag = tag+"_Muons";
+    HFolder(mytag)->fill(event);
+    mytag = tag+"_Electrons";
+    HFolder(mytag)->fill(event);
+    mytag = tag+"_Jets";
+    HFolder(mytag)->fill(event);
+    mytag = tag+"_Event";
+    HFolder(mytag)->fill(event);
+  }
+
+
+  LQTopLepTriggerPreselectionModule::LQTopLepTriggerPreselectionModule(Context & ctx){
+
+    cout << "Hello World from LQTopLepTriggerPreselectionModule!" << endl;
+    for(auto & kv : ctx.get_all()) cout << " " << kv.first << " = " << kv.second << endl;
+
+    is_mc = ctx.get("dataset_type") == "MC";
+    Year year = extract_year(ctx);
+
+
+    // Object kinematic cuts
+    // Idea: require == 1 lepton with >30 gev (minimum offline threshold for trigger plateaus), while not throwing away leptons with 10 < pt < 30 (for plotting the turn-on).
+    // lowpt for cleaning and requiring at least 1
+    // highpt for requiring == 1
+    double leptonlowpt = 10.;
+    double leptonhighpt = 30.;
+    double leptoneta = 2.4;
+    double jetpt = 30.;
+    double jeteta = 2.4;
+
+
+    // Object IDs
+    JetId jet_pfid = JetPFID(JetPFID::WP_TIGHT_CHS);
+    jetId = AndId<Jet>(jet_pfid, PtEtaCut(jetpt, jeteta));
+    eleId_lowpt = AndId<Electron>(ElectronID_Fall17_tight, PtEtaCut(leptonlowpt, leptoneta));
+    eleId_highpt = AndId<Electron>(ElectronID_Fall17_tight, PtEtaCut(leptonhighpt, leptoneta));
+    if (year == Year::is2016v2){
+      muId_lowpt = AndId<Muon>(MuonID(Muon::Tight), PtEtaCut(leptonlowpt, leptoneta), MuonIso(0.15));
+      muId_highpt = AndId<Muon>(MuonID(Muon::Tight), PtEtaCut(leptonhighpt, leptoneta), MuonIso(0.15));
+    }
+    else{
+      muId_lowpt = AndId<Muon>(MuonID(Muon::CutBasedIdTight), PtEtaCut(leptonlowpt, leptoneta), MuonID(Muon::PFIsoTight));
+      muId_highpt = AndId<Muon>(MuonID(Muon::CutBasedIdTight), PtEtaCut(leptonhighpt, leptoneta), MuonID(Muon::PFIsoTight));
+    }
+
+
+    // CommonModules
+    common.reset(new CommonModules());
+    common->switch_jetlepcleaner(true);
+    common->set_electron_id(eleId_lowpt);
+    common->set_muon_id(muId_lowpt);
+    common->set_jet_id(jetId);
+    common->switch_jetPtSorter();
+    common->init(ctx);
+
+
+    // Selections
+    nelectrons_lowpt_sel.reset(new NElectronSelection(1, -1));
+    nmuons_lowpt_sel.reset(new NMuonSelection(1, -1));
+    nelectrons_highpt_sel.reset(new NElectronSelection(1, 1, eleId_highpt));
+    nmuons_highpt_sel.reset(new NMuonSelection(1, 1, muId_highpt));
+    njets_sel.reset(new NJetSelection(2, -1));
+
+
+    // Book histograms
+    vector<string> histogram_tags = {"NoCuts", "Cleaner", "2Jets", "ElectronLowpt", "MuonLowpt", "ElectronHighpt", "MuonHighpt"};
+    book_histograms(ctx, histogram_tags);
+
+
+  }
+
+
+  bool LQTopLepTriggerPreselectionModule::process(Event & event) {
+
+
+    fill_histograms(event,"NoCuts");
+
+    bool pass_common = common->process(event);
+    if(!pass_common) return false;
+    fill_histograms(event,"Cleaner");
+
+    if(!njets_sel->passes(event)) return false;
+    fill_histograms(event,"2Jets");
+
+    if(!nelectrons_lowpt_sel->passes(event)) return false;
+    fill_histograms(event,"ElectronLowpt");
+
+    if(!nmuons_lowpt_sel->passes(event)) return false;
+    fill_histograms(event,"MuonLowpt");
+
+    if(!nelectrons_highpt_sel->passes(event)) return false;
+    fill_histograms(event,"ElectronHighpt");
+
+    if(!nmuons_highpt_sel->passes(event)) return false;
+    fill_histograms(event,"MuonHighpt");
+
+    return true;
+  }
+
+  // as we want to run the ExampleCycleNew directly with AnalysisModuleRunner,
+  // make sure the LQTopLepTriggerPreselectionModule is found by class name. This is ensured by this macro:
+  UHH2_REGISTER_ANALYSIS_MODULE(LQTopLepTriggerPreselectionModule)
+
+}
