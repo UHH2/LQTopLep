94978832d3b9a3413730df5bc5744d13be79ac23
Arne Reimers <Arne.Reimers@desy.de>
2019-12-18 14:59:13 +0100

    Created macro to calculate trigger efficiencies

---
+++ macros/include/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <TString.h>
+
+using namespace std;
+
+map<TString, TString> infotexts{
+  {"2016", "35.9 fb^{-1} (13 TeV)"},
+  {"2017", "41.5 fb^{-1} (13 TeV)"},
+  {"2018", "59.7 fb^{-1} (13 TeV)"}
+};
